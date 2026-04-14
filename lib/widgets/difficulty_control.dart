181ceb2a1f62715e9cd83e89abba1c723d0bb45e
Alfred <alfred.reintz@icloud.com>
2026-04-14 10:43:32 +0200

    added icons to difficulty levels

--- lib/widgets/difficulty_control.dart
+++ lib/widgets/difficulty_control.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lab2/app_theme.dart';
 import 'package:lab2/model/recipe_database/recipe_handler.dart';
 import 'package:lab2/util/difficulty.dart';
 import 'package:provider/provider.dart';
@@ -16,6 +17,8 @@ class _DifficulyControlState extends State<DifficulyControl> {
   Widget build(BuildContext context) {
 
     var recipeHandler = Provider.of<RecipeHandler>(context, listen: false);
+    var labels = Difficulty.labels;
+    var icons = Difficulty.icons;
 
     return RadioGroup<String>(
       groupValue: _difficulty,
@@ -27,11 +30,18 @@ class _DifficulyControlState extends State<DifficulyControl> {
       },
       child: Column(
         children: [
-          for (final label in Difficulty.labels)
+          for (int i = 0; i < labels.length; i++)
             RadioListTile<String>(
               dense: true,
-              title: Text(label),
-              value: label,
+              title: Row(
+                children: [
+                  if (icons[i] != null) ...[
+                    icons[i]!,
+                  ],
+                  Text(labels[i])
+                ]
+              ),
+              value: labels[i],
             ),
         ],
         ),
